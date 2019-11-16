c1df2d284c53e64e05a44e56333c51aa429757d7
Tomáš Černý <tmscer@gmail.com>
2019-11-16 11:07:28 +0100

    use in-memory mongo for tests

---
+++ src/db/inMemory.ts
@@ -0,0 +1,35 @@
+// https://dev.to/paulasantamaria/testing-node-js-mongoose-with-an-in-memory-database-32np
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+const mongod = new MongoMemoryServer();
+
+/**
+ * Connect to the in-memory database.
+ */
+export const connect = async options => {
+  const uri = await mongod.getConnectionString();
+
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    ...options
+  };
+
+  await mongoose.connect(uri, mongooseOpts);
+};
+
+/**
+ * Drop database, close the connection and stop mongod.
+ */
+export const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
+
+/**
+ * Remove all the data for all db collections.
+ */
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,36 @@
 import mongoose from "mongoose";
+import * as mem from "./inMemory";
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
 
-export const connect = async () => {
+const clear = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+};
+
+const normalConnect = async () => {
   await mongoose.connect("mongodb://localhost/test", options);
 };
 
-export const disconnect = async () => {
+const normalDisconnect = async () => {
   await mongoose.disconnect();
 };
+
+let connect, disconnect;
+
+if (process.env.NODE_ENV === "test") {
+  connect = async () => await mem.connect(options);
+  disconnect = mem.closeDatabase;
+} else {
+  connect = normalConnect;
+  disconnect = normalDisconnect;
+}
+
+export { connect, disconnect, clear };
