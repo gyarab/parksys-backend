ad7f5292952be3696b9cbc1a5fc3ca981c3217ae
Tomáš Černý <tmscer@gmail.com>
2019-12-07 10:48:39 +0100

    restrict User.authentications resolver to owner and root only

--- src/types/user/__tests__/user.resolvers.test.ts
+++ src/types/user/__tests__/user.resolvers.test.ts
@@ -1,34 +1,96 @@
 import { begin } from "../../../app";
 import { disconnect } from "../../../db";
 import resolvers from "../user.resolvers";
-import { User, IUser, IUserDocument } from "../user.model";
+import { User, IUserDocument } from "../user.model";
 import { Permission } from "../../permissions";
+import {
+  Authentication,
+  AuthenticationMethod
+} from "../../../types/authentication/authentication.model";
 
 describe("user query resolvers", () => {
+  let user1: IUserDocument;
+  let user2: IUserDocument;
   it("currentUser", async () => {
-    const userData: IUser = {
-      name: "user1",
-      email: "user1@example.com",
-      permissions: [Permission.ALL]
-    };
-
-    const dbUser: IUserDocument = (await User.create([userData]))[0];
-
     const user = await resolvers.Query.currentUser(null, null, {
       token: {
         user: {
-          id: dbUser.id,
-          permissions: []
+          id: user1.id,
+          permissions: user1.permissions
         }
       }
     });
 
-    expect(user._id).toStrictEqual(dbUser._id);
+    expect(user._id).toStrictEqual(user1._id);
     expect(user.name).toBe("user1");
+  });
+
+  it("User.authentications", async () => {
+    // Only owner or root can access User.authentications
+    // Owner accesses
+    const authentications1 = resolvers.User.authentications(user2, null, {
+      token: {
+        user: {
+          id: user2.id,
+          permissions: user2.permissions
+        }
+      }
+    });
+    expect(authentications1).toHaveLength(1);
+    expect(authentications1[0]).toMatchObject(user2.authentications[0]);
+
+    // Root accesses
+    const authentications2 = resolvers.User.authentications(user2, null, {
+      token: {
+        user: {
+          id: user1.id,
+          permissions: user1.permissions
+        }
+      }
+    });
+    expect(authentications2).toHaveLength(1);
+    expect(authentications2[0]).toMatchObject(user2.authentications[0]);
+
+    // Error
+    const authentications3 = () => {
+      resolvers.User.authentications(user1, null, {
+        token: {
+          user: {
+            id: user2.id,
+            permissions: user2.permissions
+          }
+        }
+      });
+    };
+    expect(authentications3).toThrowError();
   });
 
   beforeAll(async () => {
     await begin();
+    [user1, user2] = await User.create([
+      {
+        name: "user1",
+        email: "user1@example.com",
+        permissions: [Permission.ALL],
+        authentications: [
+          new Authentication({
+            method: AuthenticationMethod.PASSWORD,
+            payload: "123"
+          })
+        ]
+      },
+      {
+        name: "user2",
+        email: "user2@example.com",
+        permissions: [],
+        authentications: [
+          new Authentication({
+            method: AuthenticationMethod.PASSWORD,
+            payload: "456"
+          })
+        ]
+      }
+    ]);
     // Create any required models
   });
 
--- src/types/user/user.resolvers.ts
+++ src/types/user/user.resolvers.ts
@@ -1,9 +1,10 @@
-import { User } from "./user.model";
+import { User, IUserDocument } from "./user.model";
 import lodash from "lodash";
 import { checkPermissionsGqlBuilder } from "../../auth/auth";
 import { Permission } from "../permissions";
 import { Resolver } from "../../db/gql";
 
+// Query
 const currentUser: Resolver = async (_, args, ctx) => {
   const uid = lodash.get(ctx, "token.user.id");
   if (uid) {
@@ -17,10 +18,27 @@ const users: Resolver = async (_, args, ctx) => {
   return await User.find({});
 };
 
+// User
+const authentications: Resolver = (obj: IUserDocument, _, ctx) => {
+  const uid = lodash.get(ctx, "token.user.id");
+  const permissions: Array<String> = lodash.get(
+    ctx,
+    "token.user.permissions",
+    []
+  );
+  if (uid === obj._id.toString() || permissions.includes(Permission.ALL)) {
+    return obj.authentications;
+  } else {
+    throw new Error("Only owner and root can request authentications");
+  }
+};
+
 export default {
   Query: {
     currentUser,
     users: checkPermissionsGqlBuilder([Permission.ALL], users)
   },
-  User: {}
+  User: {
+    authentications
+  }
 };
