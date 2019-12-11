0178f6ab71b1c93721e09e781a9636fbbada219a
Tomáš Černý <tmscer@gmail.com>
2019-12-11 14:09:04 +0100

    vehicle model test

---
+++ src/types/vehicle/__tests__/vehicle.model.test.ts
@@ -0,0 +1,22 @@
+import { Vehicle } from "../vehicle.model";
+
+describe("model Vehicle", () => {
+  it("requires correct fields", async () => {
+    const veh1 = new Vehicle({
+      licensePlate: "123F4567"
+    });
+    try {
+      await veh1.validate();
+    } catch (e) {
+      fail(e);
+    }
+    const veh2 = new Vehicle();
+    try {
+      await veh2.validate();
+      fail("expected an error");
+    } catch (err) {
+      expect(err).not.toBeNull();
+      expect(err.errors.licensePlate).toBeDefined();
+    }
+  });
+});
