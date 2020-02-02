9d521f43841b3e662277336f340a538c60851209
Tomáš Černý <tmscer@gmail.com>
2020-02-02 14:53:46 +0100

    hourly stats for a day

--- src/types/stats/stats.resolvers.ts
+++ src/types/stats/stats.resolvers.ts
@@ -1,10 +1,12 @@
 import { Resolver } from "../../db/gql";
-import moment from "moment";
+import moment, { min } from "moment";
 
 // Query
+// TODO: Range selection
 const dayStats: Resolver = async (_, args, ctx, info) => {
   // Calculate today
   const startDate: Date = moment()
+    .subtract(2, "weeks")
     .startOf("day")
     .toDate();
   // Constants
@@ -42,17 +44,68 @@ const dayStats: Resolver = async (_, args, ctx, info) => {
     }
   ];
   const result = await ctx.models.ParkingSession.aggregate(aggr);
-  return result.map(dayData => {
-    const id = dayData._id;
+  return result.map(({ _id: id, numParkingSessions, revenueCents }) => {
     const month = String(id.month).padStart(2, "0");
     const date = String(id.date).padStart(2, "0");
-    dayData.day = `${id.year}-${month}-${date}`;
-    return dayData;
+    const day = `${id.year}-${month}-${date}`;
+    return {
+      day,
+      data: {
+        numParkingSessions,
+        revenueCents
+      }
+    };
   });
+};
+
+const dayStatsPerHour: Resolver = async (_, args, ctx) => {
+  const dateField = "checkOut.time";
+  const dateField$ = `$${dateField}`;
+  const timezone = "Europe/Prague";
+  // TODO: Catch invalid inputs (timestamp, iso date, etc.)
+  const day = moment(args.day);
+  const start: Date = day.startOf("day").toDate();
+  const end: Date = day.endOf("day").toDate();
+  if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+    // Invalid date
+    throw new Error("Supplied day is not of YYYY-MM-DD format");
+  }
+  const aggr = [
+    {
+      $match: {
+        [dateField]: { $gte: start, $lte: end }
+      }
+    },
+    {
+      $group: {
+        _id: {
+          $hour: { date: dateField$, timezone: timezone }
+        },
+        revenueCents: { $sum: "$finalFee" },
+        numParkingSessions: { $sum: 1 }
+      }
+    },
+    {
+      $project: {
+        hour: "$_id",
+        data: {
+          revenueCents: "$revenueCents",
+          numParkingSessions: { $toInt: "$numParkingSessions" }
+        }
+      }
+    }
+  ];
+  const results = await ctx.models.ParkingSession.aggregate(aggr);
+  console.log(results);
+  return {
+    day: args.day,
+    data: results
+  };
 };
 
 export default {
   Query: {
-    dayStats
+    dayStats,
+    dayStatsPerHour
   }
 };
